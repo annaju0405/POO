a53c4f4ee6bcca30bc28f145677f7252321320c5
Anna Julia Curcio Marques <134973253+annaju0405@users.noreply.github.com>
2024-09-12 10:05:37 -0300

    Create ItemMenu.java

---
+++ ItemMenu.java
@@ -0,0 +1,28 @@
+public class ItemMenu {
+    private String nome;
+    private String categoria;
+    private double preco;
+
+    public ItemMenu(String nome, String categoria, double preco) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Categoria: " + categoria + ", Preço: R$" + preco;
+    }
+}
